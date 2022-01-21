62346bfded52d7c66ec2f9c7ae7652949312dde9
jhLim97 <wnsgur2749@naver.com>
2022-01-21 22:08:19 +0900

    chore: 웹팩 기본설정 - mode, entry, output 설정

---
+++ src/index.js
@@ -0,0 +1,8 @@
+function component() {
+  var element = document.createElement("div");
+  element.innerHTML = "React Boilerplate";
+  return element;
+}
+
+const root = document.querySelector("#root");
+root.appendChild(component());
