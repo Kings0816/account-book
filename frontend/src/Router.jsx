1c87f63492746579bac229429bbdf867259be862
jhLim97 <wnsgur2749@naver.com>
2022-01-22 23:37:44 +0900

    feat: 로그인페이지 라우팅 추가

--- frontend/src/Router.jsx
+++ frontend/src/Router.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Login from './pages/Login';
 import Main from './pages/Main';
 import Calendar from './pages/Calendar';
 import Statistics from './pages/Statistics';
@@ -9,6 +10,7 @@ const Router = () => {
         <BrowserRouter>
             <Routes>
                 <Route exact path="/" element={<Main />} />
+                <Route exact path="/login" element={<Login />} />
                 <Route exact path="/calendar" element={<Calendar />} />
                 <Route exact path="/statistics" element={<Statistics />} />
             </Routes>
