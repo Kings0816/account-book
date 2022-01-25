9f472416c10a7cc037ca4efd8526cc3f85cb7318
jhLim97 <wnsgur2749@naver.com>
2022-01-25 22:44:32 +0900

    feat: 인증이 필요한 페이지에 대한 AuthRouter 설정 추가
    * 인증이 필요한 페이지에 대해 AuthRouter 컴포넌트를 만들어서
      적용했습니다.
    * AuthRouter에서는 현재 인증이 되어있는 상태인지 확인 후 상태에 맞는
      경로로 이동합니다.

--- frontend/src/Router.jsx
+++ frontend/src/Router.jsx
@@ -1,21 +1,45 @@
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
 import Main from './pages/Main';
 import Login from './pages/Login';
 import Calendar from './pages/Calendar';
 import Statistics from './pages/Statistics';
 import GithubOauth from './pages/GithubOauth';
 
+import AuthRoute from './components/AuthRoute';
+
 const Router = () => {
     return (
         <BrowserRouter>
             {/* TODO 설정된 경로 이외의 경로에 대한 Not Found 페이지 렌더링 */}
             <Routes>
-                <Route path="/" element={<Main />} />
                 <Route path="/login" element={<Login />} />
-                <Route path="/calendar" element={<Calendar />} />
-                <Route path="/statistics" element={<Statistics />} />
                 <Route path="/auth" element={<GithubOauth />} />
+                <Route
+                    path="/"
+                    element={
+                        <AuthRoute>
+                            <Main />
+                        </AuthRoute>
+                    }
+                />
+                <Route
+                    path="/calendar"
+                    element={
+                        <AuthRoute>
+                            <Calendar />
+                        </AuthRoute>
+                    }
+                />
+                <Route
+                    path="/statistics"
+                    element={
+                        <AuthRoute>
+                            <Statistics />
+                        </AuthRoute>
+                    }
+                />
             </Routes>
         </BrowserRouter>
     );
