dd178d735884d6ef26b900862bd943ea8fa14fd0
jhLim97 <wnsgur2749@naver.com>
2022-01-30 02:02:11 +0900

    feat: props 타입 지정

--- frontend/src/components/AuthRoute/index.jsx
+++ frontend/src/components/AuthRoute/index.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import { Navigate } from 'react-router-dom';
+import PropTypes from 'prop-types';
 
-// eslint-disable-next-line react/prop-types
 const AuthRoute = ({ children }) => {
     const userJwt = sessionStorage.getItem('userJwt');
     return userJwt ? children : <Navigate replace to="/login" />;
+};
+
+AuthRoute.propTypes = {
+    children: PropTypes.node.isRequired,
 };
 
 export default AuthRoute;
