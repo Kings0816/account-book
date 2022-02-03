78f28dbe5253ac3aef364e1e3458a60d9a890e6e
jhLim97 <wnsgur2749@naver.com>
2022-02-03 20:35:11 +0900

    test: 체크박스 클릭 시 상태가 변하는 테스트코드 작성
    * 체크박스의 상태는 recoil 전역 상태로 관리되고 있습니다.
    * 체크박스 클릭 시 상태가 변경되는지 여부를 테스트하는 코드를
      작성했습니다.

--- frontend/src/components/Summary/test.js
+++ frontend/src/components/Summary/test.js
@@ -36,4 +36,37 @@ describe('메인 화면(지출입 내역) Summary 컴포넌트 테스트', () =>
 
         expect(expenditureSpan).toHaveTextContent('259,200');
     });
+
+    it('총 이체 체크박스를 클릭하면 체크박스 상태가 변경된다.', async () => {
+        render(<Summary />);
+
+        const totalCountCheckBox = await screen.findByRole('checkbox', { name: '총 이체' });
+        expect(totalCountCheckBox.checked).toEqual(true);
+
+        fireEvent.click(totalCountCheckBox);
+
+        expect(totalCountCheckBox.checked).toEqual(false);
+    });
+
+    it('수입 체크박스를 클릭하면 체크박스 상태가 변경된다.', async () => {
+        render(<Summary />);
+
+        const incomeCheckBox = await screen.findByRole('checkbox', { name: '수입' });
+        expect(incomeCheckBox.checked).toEqual(true);
+
+        fireEvent.click(incomeCheckBox);
+
+        expect(incomeCheckBox.checked).toEqual(false);
+    });
+
+    it('지출 체크박스를 클릭하면 체크박스 상태가 변경된다.', async () => {
+        render(<Summary />);
+
+        const expenditureCheckBox = await screen.findByRole('checkbox', { name: '지출' });
+        expect(expenditureCheckBox.checked).toEqual(true);
+
+        fireEvent.click(expenditureCheckBox);
+
+        expect(expenditureCheckBox.checked).toEqual(false);
+    });
 });
