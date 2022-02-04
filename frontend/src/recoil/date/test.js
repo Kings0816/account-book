677920d73b83281f949600f732088f39bb01e389
jhLim97 <wnsgur2749@naver.com>
2022-02-04 20:54:29 +0900

    test: date의 transactionsInDateState selector에 대한 날짜에 해당하는 거래내역 로직 테스트코드 작성

--- frontend/src/recoil/date/test.js
+++ frontend/src/recoil/date/test.js
@@ -14,4 +14,14 @@ describe('date Selectors 테스트', () => {
         const shapedDate = `${initialState.year}년 ${initialState.month}월`;
         expect(initialSnapshot.getLoadable(shapedDateState).valueOrThrow()).toBe(shapedDate);
     });
+
+    it('transactionsInDateState(현재 날짜에 해당하는 거래내역을 반환) Selector 테스트', async () => {
+        const testSnapshot = snapshot_UNSTABLE(({ set }) =>
+            set(dateState, { year: 2022, month: 1 }),
+        );
+        const transactionsInDate = await testSnapshot
+            .getLoadable(transactionsInDateState)
+            .promiseOrThrow();
+        expect(transactionsInDate).toEqual(transactionsWithDate['2022-1']);
+    });
 });
