9466c25284f6f5fb134f62e64b2e3060fe2bdc86
jhLim97 <wnsgur2749@naver.com>
2022-02-04 18:43:24 +0900

    test: 수입, 지출 내역을 계산하는 util 함수 테스트코드 작성

--- frontend/src/utils/common/day-info.js
+++
@@ -1,8 +0,0 @@
-import { WEEK_DAY } from '../constant/week';
-
-export const dayInfo = (date) => {
-    const [year, month, day] = date.split('-');
-    const week = new Date(year, month, day).getDay();
-    const weekDay = WEEK_DAY[week];
-    return { month, day, weekDay };
-};
