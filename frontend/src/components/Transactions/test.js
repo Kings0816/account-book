0a8aa29a4efc84f1b1d086ea92fcfc4e483f0e87
jhLim97 <wnsgur2749@naver.com>
2022-02-04 18:35:08 +0900

    test: transactions 컴포넌트 체크박스 상태에 따라 필터링이 적용된 거래내역이 표시되는 로직에 대한 테스트코드 작성

---
+++ frontend/src/components/Transactions/test.js
@@ -0,0 +1,97 @@
+import React, { Suspense } from 'react';
+import { RecoilRoot } from 'recoil';
+import { ThemeProvider } from 'styled-components';
+import { render, screen, waitFor } from '@testing-library/react';
+import '@testing-library/jest-dom';
+
+import { checkState } from '../../recoil/check/atom';
+import theme from '../../styles/theme';
+import Transactions from '.';
+
+const TOTAL_COUNT = 14;
+const INCOME_COUNT = 1;
+const EXPENDITURE_COUNT = 13;
+
+describe('해당 날짜(월 단위)의 모든 거래내역을 표시하는 Transactions 컴포넌트 테스트', () => {
+    const initializeState =
+        (value) =>
+        ({ set }) => {
+            set(checkState, {
+                totalCount: value.totalCount,
+                income: value.income,
+                expenditure: value.expenditure,
+            });
+        };
+
+    it('지출 내역의 check box 상태가 false인 경우 지출 내역이 필터링되어 표시된다.', async () => {
+        // TODO test-utils 파일과 합치기
+        render(
+            <RecoilRoot
+                initializeState={initializeState({
+                    totalCount: true,
+                    income: true,
+                    expenditure: false,
+                })}
+            >
+                <ThemeProvider theme={theme}>
+                    <Suspense fallback={<div>Loading...</div>}>
+                        <Transactions />
+                    </Suspense>
+                </ThemeProvider>
+            </RecoilRoot>,
+        );
+
+        const filterdTransactions = await screen.findAllByRole('listitem');
+
+        const result = TOTAL_COUNT - EXPENDITURE_COUNT;
+        expect(filterdTransactions).toHaveLength(result);
+    });
+
+    it('수입 내역의 check box 상태가 false인 경우 수입 내역이 필터링되어 표시된다.', async () => {
+        // TODO test-utils 파일과 합치기
+        render(
+            <RecoilRoot
+                initializeState={initializeState({
+                    totalCount: true,
+                    income: false,
+                    expenditure: true,
+                })}
+            >
+                <ThemeProvider theme={theme}>
+                    <Suspense fallback={<div>Loading...</div>}>
+                        <Transactions />
+                    </Suspense>
+                </ThemeProvider>
+            </RecoilRoot>,
+        );
+
+        const filterdTransactions = await screen.findAllByRole('listitem');
+
+        const result = TOTAL_COUNT - INCOME_COUNT;
+        expect(filterdTransactions).toHaveLength(result);
+    });
+
+    it('지출, 수입 내역의 check box 상태가 false인 경우 0건이 표시된다.', async () => {
+        // TODO test-utils 파일과 합치기
+        render(
+            <RecoilRoot
+                initializeState={initializeState({
+                    totalCount: true,
+                    income: false,
+                    expenditure: false,
+                })}
+            >
+                <ThemeProvider theme={theme}>
+                    <Suspense fallback={<div>Loading...</div>}>
+                        <Transactions />
+                    </Suspense>
+                </ThemeProvider>
+            </RecoilRoot>,
+        );
+
+        const filterdTransactions = await waitFor(() => screen.queryAllByRole('listitem'));
+
+        const result = TOTAL_COUNT - INCOME_COUNT - EXPENDITURE_COUNT;
+        expect(filterdTransactions).toHaveLength(result);
+    });
+});
