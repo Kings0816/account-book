a64d67f1730565dc579489d6885265be11cd4a55
jhLim97 <wnsgur2749@naver.com>
2022-02-06 04:35:43 +0900

    refactor: custom render 활용하도록 변경

--- frontend/src/components/Transactions/test.js
+++ frontend/src/components/Transactions/test.js
@@ -1,12 +1,10 @@
-import React, { Suspense } from 'react';
+import React from 'react';
 import { RecoilRoot } from 'recoil';
-import { ThemeProvider } from 'styled-components';
-import { render, screen, waitFor } from '@testing-library/react';
+import { render, screen, waitFor } from '../../test-utils';
 import '@testing-library/jest-dom';
 
 import Transactions from '.';
 import { checkState } from '../../recoil/check/atom';
-import theme from '../../styles/theme';
 import { transactions } from '../../mocks/transactions';
 
 const TOTAL_COUNT = transactions.length;
@@ -25,73 +23,52 @@ describe('해당 날짜(월 단위)의 모든 거래내역을 표시하는 Trans
         };
 
     it('지출 내역의 check box 상태가 false인 경우 지출 내역이 필터링되어 표시된다.', async () => {
-        // TODO test-utils 파일과 합치기
+        const TEST_DATA = {
+            totalCount: true,
+            income: true,
+            expenditure: false,
+        };
         render(
-            <RecoilRoot
-                initializeState={initializeState({
-                    totalCount: true,
-                    income: true,
-                    expenditure: false,
-                })}
-            >
-                <ThemeProvider theme={theme}>
-                    <Suspense fallback={<div>Loading...</div>}>
-                        <Transactions />
-                    </Suspense>
-                </ThemeProvider>
+            <RecoilRoot initializeState={initializeState(TEST_DATA)}>
+                <Transactions />
             </RecoilRoot>,
         );
 
         const filterdTransactions = await screen.findAllByRole('listitem');
-
         const result = TOTAL_COUNT - EXPENDITURE_COUNT;
         expect(filterdTransactions).toHaveLength(result);
     });
 
     it('수입 내역의 check box 상태가 false인 경우 수입 내역이 필터링되어 표시된다.', async () => {
-        // TODO test-utils 파일과 합치기
+        const TEST_DATA = {
+            totalCount: true,
+            income: false,
+            expenditure: true,
+        };
         render(
-            <RecoilRoot
-                initializeState={initializeState({
-                    totalCount: true,
-                    income: false,
-                    expenditure: true,
-                })}
-            >
-                <ThemeProvider theme={theme}>
-                    <Suspense fallback={<div>Loading...</div>}>
-                        <Transactions />
-                    </Suspense>
-                </ThemeProvider>
+            <RecoilRoot initializeState={initializeState(TEST_DATA)}>
+                <Transactions />
             </RecoilRoot>,
         );
 
         const filterdTransactions = await screen.findAllByRole('listitem');
-
         const result = TOTAL_COUNT - INCOME_COUNT;
         expect(filterdTransactions).toHaveLength(result);
     });
 
     it('지출, 수입 내역의 check box 상태가 false인 경우 0건이 표시된다.', async () => {
-        // TODO test-utils 파일과 합치기
+        const TEST_DATA = {
+            totalCount: true,
+            income: false,
+            expenditure: false,
+        };
         render(
-            <RecoilRoot
-                initializeState={initializeState({
-                    totalCount: true,
-                    income: false,
-                    expenditure: false,
-                })}
-            >
-                <ThemeProvider theme={theme}>
-                    <Suspense fallback={<div>Loading...</div>}>
-                        <Transactions />
-                    </Suspense>
-                </ThemeProvider>
+            <RecoilRoot initializeState={initializeState(TEST_DATA)}>
+                <Transactions />
             </RecoilRoot>,
         );
 
         const filterdTransactions = await waitFor(() => screen.queryAllByRole('listitem'));
-
         const result = TOTAL_COUNT - INCOME_COUNT - EXPENDITURE_COUNT;
         expect(filterdTransactions).toHaveLength(result);
     });
