77290e10655a8db8c52bb9107545c49d944be8d3
jhLim97 <wnsgur2749@naver.com>
2022-02-06 05:12:12 +0900

    일자별로 데이터를 분리하는 로직 util 함수로 추출

--- frontend/src/components/Transactions/index.jsx
+++ frontend/src/components/Transactions/index.jsx
@@ -7,19 +7,12 @@ import TransactionModal from '../TransactionModal';
 import { useFilterdTransactions } from './hooks';
 import { checkState } from '../../recoil/check/atom';
 import { TransactionsContainer } from './style';
+import { makeDailyTransaction } from '../../utils/common/make-daily-transaction';
 
 const Transactions = () => {
     const check = useRecoilValue(checkState);
     const filterdTransactions = useFilterdTransactions(check);
-
-    const dailyTransactions = new Map();
-
-    filterdTransactions.forEach((rawTransaction) => {
-        const currentDate = rawTransaction.date;
-        if (dailyTransactions.has(currentDate)) {
-            dailyTransactions.get(currentDate).push(rawTransaction);
-        } else dailyTransactions.set(currentDate, [rawTransaction]);
-    });
+    const dailyTransactions = makeDailyTransaction(filterdTransactions);
 
     const shapedTransactions = Array.from(dailyTransactions.keys())
         .sort((a, b) => new Date(b) - new Date(a))
