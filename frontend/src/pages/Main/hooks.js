7001c299dc99dfff8471192fa698d839273be66f
jhLim97 <wnsgur2749@naver.com>
2022-02-08 16:37:49 +0900

    feat: 메인페이지 진입 시 카테고리를 요청하고 atom에 저장하는 로직 구현

--- frontend/src/pages/Main/hooks.js
+++ frontend/src/pages/Main/hooks.js
@@ -1,15 +1,23 @@
 import { useSetRecoilState } from 'recoil';
 
 import { getMethods } from '../../lib/method';
+import { getCategories } from '../../lib/category';
 import { methodState } from '../../recoil/method/atom';
+import { categoryState } from '../../recoil/category/atom';
 
-export const usefetchMethods = () => {
+export const usefetchMethodAndCategory = () => {
     const setMethod = useSetRecoilState(methodState);
+    const setCategory = useSetRecoilState(categoryState);
 
-    const fetchMethods = async () => {
+    const fetchMethod = async () => {
         const result = await getMethods();
         result && setMethod(result.data);
     };
 
-    return [fetchMethods];
+    const fetchCategory = async () => {
+        const result = await getCategories();
+        result && setCategory(result.data);
+    };
+
+    return [fetchMethod, fetchCategory];
 };
