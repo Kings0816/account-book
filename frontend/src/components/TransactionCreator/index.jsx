77ca3f013957e96c43090fcf3102a1e4e7e6f0ce
jhLim97 <wnsgur2749@naver.com>
2022-02-10 20:37:18 +0900

    feat: 거래내역 추가 버튼 클릭 시 모달상태에 추가되는 로직 구현

--- frontend/src/components/TransactionCreator/index.jsx
+++ frontend/src/components/TransactionCreator/index.jsx
@@ -1,18 +1,31 @@
 import React from 'react';
 
+import { useModal } from '../../hooks/useModal';
 import fabImg from '../../../public/assets/fab-button.svg';
 import { CreatorContainer, Creator, Fab } from './style';
 
+const initData = {
+    category: '',
+    color: '',
+    content: '',
+    method: '',
+    sign: '+',
+    cost: '',
+    date: '',
+};
+
 const TransactionCreator = () => {
+    const { openModal } = useModal();
+
     return (
         <>
             <CreatorContainer>
-                <Creator type="button" onClick={() => console.log(111)}>
+                <Creator type="button" onClick={() => openModal('createTransaction', initData)}>
                     내역 추가하기
                 </Creator>
             </CreatorContainer>
             <Fab>
-                <button type="button" onClick={() => console.log(111)}>
+                <button type="button" onClick={() => openModal('createTransaction', initData)}>
                     <img src={fabImg} />
                 </button>
             </Fab>
