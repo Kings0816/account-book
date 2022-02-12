d4db0bc48a83422789057d7bda9cd2e237b3931b
jhLim97 <wnsgur2749@naver.com>
2022-02-12 21:39:12 +0900

    test: 잘못된 테스트코드 수정

--- frontend/src/utils/common/calculate-cost/test.js
+++ frontend/src/utils/common/calculate-cost/test.js
@@ -7,7 +7,7 @@ const EXPENDITURE = 259200;
 describe('수입, 지출 금액 계산 테스트', () => {
     it('수입 거래내역에 대한 총 금액을 반환한다.', () => {
         const result = calculateIncome(transactions);
-        expect(result).toEqual(3000);
+        expect(result).toEqual(INCOME);
     });
 
     it('지출 거래내역에 대한 총 금액을 반환한다.', () => {
