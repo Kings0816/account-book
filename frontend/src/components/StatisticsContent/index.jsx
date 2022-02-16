90e4488a1fef759ac3a1f6b683076e45c6fd2f82
jhLim97 <wnsgur2749@naver.com>
2022-02-17 04:34:54 +0900

    feat: 특정 카테고릴 선택 시 해당 카테고리의 내역들을 렌더링하는 모달을 여는 로직 구현
    * 특정 카테고리 클릭 시 모달 커스텀 hook을 활용해 거래내역 모달이
      열리도록 했습니다.
    * 특정 카테고리 선택 시 이전에 선택된 모달은 닫히도록 구현했습니다.

--- frontend/src/components/StatisticsContent/index.jsx
+++ frontend/src/components/StatisticsContent/index.jsx
@@ -1,18 +1,27 @@
 import React from 'react';
 import { nanoid } from 'nanoid';
 
+import { useModal } from '../../hooks/useModal';
 import { Wrapper, OuterBox, InnerBox, Category, Content, Cost } from './style';
 
 const [CATEGORY_INDEX, COST_INDEX] = [0, 1];
 
 const StatisticsContent = ({ transactionsByCategory }) => {
+    const { openModal, closeModal } = useModal();
+
+    const changeCategory = (transactions) => {
+        closeModal('categoryTransaction');
+        openModal('categoryTransaction', transactions);
+    };
+
     const summaryByCategory = Array.from(transactionsByCategory.entries())
         .sort((a, b) => b[COST_INDEX].percent - a[COST_INDEX].percent)
         .map((percentWithCategory) => {
             const { name, color } = percentWithCategory[CATEGORY_INDEX];
-            const { percent, expenditureInCategory } = percentWithCategory[COST_INDEX];
+            const { transactions, percent, expenditureInCategory } =
+                percentWithCategory[COST_INDEX];
             return expenditureInCategory !== 0 ? (
-                <OuterBox key={nanoid()}>
+                <OuterBox key={nanoid()} onClick={() => changeCategory(transactions)}>
                     <InnerBox>
                         <Category color={color}>{name}</Category>
                         <Content>{Math.ceil(percent)}%</Content>
