6a09ab609888d1bbb136403714f3a5d2b0e480af
jhLim97 <wnsgur2749@naver.com>
2022-02-16 18:09:32 +0900

    refactor: 도넛차트 컴포넌트 분리

--- frontend/src/pages/Statistics/index.jsx
+++ frontend/src/pages/Statistics/index.jsx
@@ -1,15 +1,12 @@
 import React, { Suspense } from 'react';
 import { useRecoilValue } from 'recoil';
-import { nanoid } from 'nanoid';
 
 import Header from '../../components/Header';
+import Donut from '../../components/Donut';
 import { useTransactionSummary } from '../../hooks/useTransactionSummary';
 import { filteredCategoryState } from '../../recoil/category/selector';
 import { calculateExpenditure } from '../../utils/common/calculate-cost';
-import { MainWrapper, DonutCircle } from './style';
-
-const CATEGORY_INDEX = 0;
-const COST_INDEX = 1;
+import { MainWrapper } from './style';
 
 const Statistics = () => {
     const categories = useRecoilValue(filteredCategoryState);
@@ -25,44 +22,11 @@ const Statistics = () => {
         transactionsByCategory.set(category, { percent, expenditureInCategory });
     });
 
-    const [cx, cy, r, width, startAngle] = [50, 50, 30, 15, -90];
-    const dashArray = 2 * Math.PI * r;
-    const animationDuration = 1000;
-    let accumulatePercent = 0;
-    const circles = Array.from(transactionsByCategory.entries())
-        .sort((a, b) => b[COST_INDEX].percent - a[COST_INDEX].percent)
-        .map((percentWithCategory) => {
-            const color = percentWithCategory[CATEGORY_INDEX].color;
-            const percent = percentWithCategory[COST_INDEX].percent;
-
-            const dashOffset = dashArray - (dashArray * percent) / 100;
-            const angle = (accumulatePercent * 360) / 100 + startAngle;
-            const currentDuration = (animationDuration * percent) / 100;
-            const delay = (animationDuration * accumulatePercent) / 100;
-            accumulatePercent += percent;
-            return (
-                <DonutCircle
-                    key={nanoid()}
-                    cx={cx}
-                    cy={cy}
-                    r={r}
-                    fill={'transparent'}
-                    color={color}
-                    width={width}
-                    dashArray={dashArray}
-                    dashOffset={dashOffset}
-                    angle={angle}
-                    currentDuration={currentDuration}
-                    delay={delay}
-                />
-            );
-        });
-
     return (
         <Suspense fallback={<div>Loading...</div>}>
             <Header current={'statistics'} />
             <MainWrapper>
-                <svg viewBox="0 0 100 100">{circles}</svg>
+                <Donut transactionsByCategory={transactionsByCategory} />
             </MainWrapper>
         </Suspense>
     );
--- frontend/src/pages/Statistics/style.js
+++ frontend/src/pages/Statistics/style.js
@@ -1,4 +1,4 @@
-import styled, { keyframes } from 'styled-components';
+import styled from 'styled-components';
 
 export const MainWrapper = styled.div`
     width: 100vw;
@@ -9,27 +9,4 @@ export const MainWrapper = styled.div`
     flex-direction: column;
     justify-content: flex-start;
     align-items: center;
-`;
-
-const draw = (dashOffset) => keyframes`
-    100% {
-            stroke-dashoffset: ${dashOffset};
-    }
-`;
-
-export const DonutCircle = styled.circle`
-    cx: ${(props) => props.cx};
-    cy: ${(props) => props.cy};
-    r: ${(props) => props.r};
-    fill: ${(props) => props.fill};
-
-    stroke: ${(props) => props.color};
-    stroke-width: ${(props) => props.width};
-    stroke-dasharray: ${(props) => props.dashArray};
-    stroke-dashoffset: ${(props) => props.dashArray};
-    transform: rotate(${(props) => props.angle}deg);
-    transform-origin: ${(props) => props.cx}px ${(props) => props.cy}px;
-
-    animation: ${(props) => draw(props.dashOffset)} ${(props) => props.currentDuration}ms
-        ${(props) => props.delay}ms linear forwards;
 `;
