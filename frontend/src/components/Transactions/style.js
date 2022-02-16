b2e0f15099e370208b9417e4b6445ea645501cf0
jhLim97 <wnsgur2749@naver.com>
2022-02-17 04:34:13 +0900

    chore: transactions 컴포넌트 width 설정 추가

--- frontend/src/components/Transactions/style.js
+++ frontend/src/components/Transactions/style.js
@@ -4,7 +4,7 @@ import { MAX_MOBILE_DEVICE } from '../../utils/constant/device-size';
 
 export const TransactionsContainer = styled.ul`
     margin-top: 20px;
-    width: 100%;
+    width: ${(props) => (props.width ? props.width : 100)}%;
     height: calc(100% - 150px);
 
     @media screen and (max-width: ${MAX_MOBILE_DEVICE}px) {
