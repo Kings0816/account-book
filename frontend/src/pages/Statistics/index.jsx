069efe5846b58d10123431bcffa02c4f2d764d0c
jhLim97 <wnsgur2749@naver.com>
2022-02-16 17:23:51 +0900

    feat: 실제 데이터 기반으로 도넛차트를 그리는 로직 구현
    * 현재날짜에 해당하는 거래내역들, 지출금액을 가져오는 로직을 커스텀 hook을 통해 재활용했습니다.
    * percent가 가장 높은 순서대로 도넛차트가 그려집니다.

--- frontend/src/pages/Statistics/index.jsx
+++ frontend/src/pages/Statistics/index.jsx
@@ -1,56 +1,62 @@
 import React, { Suspense } from 'react';
+import { useRecoilValue } from 'recoil';
 import { nanoid } from 'nanoid';
 
 import Header from '../../components/Header';
+import { useTransactionSummary } from '../../hooks/useTransactionSummary';
+import { filteredCategoryState } from '../../recoil/category/selector';
+import { calculateExpenditure } from '../../utils/common/calculate-cost';
 import { MainWrapper, DonutCircle } from './style';
 
+const CATEGORY_INDEX = 0;
+const PERCENT_INDEX = 1;
+
 const Statistics = () => {
-    const datas = [
-        {
-            percent: 37,
-            color: '#80e080',
-        },
-        {
-            percent: 33,
-            color: '#4fc3f7',
-        },
-        {
-            percent: 20,
-            color: '#9575cd',
-        },
-        {
-            percent: 10,
-            color: '#f06292',
-        },
-    ];
+    const categories = useRecoilValue(filteredCategoryState);
+    const { transactions, monthExpenditure } = useTransactionSummary();
+
+    const transactionsByCategory = new Map();
+    categories.forEach((category) => {
+        const transactionsInCategory = transactions.filter(
+            (transaction) => transaction.category === category.name,
+        );
+        const expenditureInCategory = calculateExpenditure(transactionsInCategory);
+        const percent = (expenditureInCategory / monthExpenditure) * 100;
+        transactionsByCategory.set(category, percent);
+    });
 
     const [cx, cy, r, width, startAngle] = [50, 50, 30, 15, -90];
     const dashArray = 2 * Math.PI * r;
     const animationDuration = 1000;
     let accumulatePercent = 0;
-    const circles = datas.map((data) => {
-        const dashOffset = dashArray - (dashArray * data.percent) / 100;
-        const angle = (accumulatePercent * 360) / 100 + startAngle;
-        const currentDuration = (animationDuration * data.percent) / 100;
-        const delay = (animationDuration * accumulatePercent) / 100;
-        accumulatePercent += data.percent;
-        return (
-            <DonutCircle
-                key={nanoid()}
-                cx={cx}
-                cy={cy}
-                r={r}
-                fill={'transparent'}
-                color={data.color}
-                width={width}
-                dashArray={dashArray}
-                dashOffset={dashOffset}
-                angle={angle}
-                currentDuration={currentDuration}
-                delay={delay}
-            />
-        );
-    });
+    const circles = Array.from(transactionsByCategory.entries())
+        .sort((a, b) => b[PERCENT_INDEX] - a[PERCENT_INDEX])
+        .map((percentWithCategory) => {
+            const color = percentWithCategory[CATEGORY_INDEX].color;
+            const percent = percentWithCategory[PERCENT_INDEX];
+
+            const dashOffset = dashArray - (dashArray * percent) / 100;
+            const angle = (accumulatePercent * 360) / 100 + startAngle;
+            const currentDuration = (animationDuration * percent) / 100;
+            const delay = (animationDuration * accumulatePercent) / 100;
+            accumulatePercent += percent;
+            return (
+                <DonutCircle
+                    key={nanoid()}
+                    cx={cx}
+                    cy={cy}
+                    r={r}
+                    fill={'transparent'}
+                    color={color}
+                    width={width}
+                    dashArray={dashArray}
+                    dashOffset={dashOffset}
+                    angle={angle}
+                    currentDuration={currentDuration}
+                    delay={delay}
+                />
+            );
+        });
 
     return (
         <Suspense fallback={<div>Loading...</div>}>
