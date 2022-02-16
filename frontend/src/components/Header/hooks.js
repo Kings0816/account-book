db6ad44cbe01acc6312f0ba154a97d31cc9729e5
jhLim97 <wnsgur2749@naver.com>
2022-02-17 04:38:25 +0900

    fix: 날짜 변경 시 이전의 선택된 카테고리의 해당하는 모달이 계속 열리는 이슈 해결

--- frontend/src/components/Header/hooks.js
+++ frontend/src/components/Header/hooks.js
@@ -1,14 +1,17 @@
 import { useRecoilState } from 'recoil';
 
+import { useModal } from '../../hooks/useModal';
 import { shapedDateState } from '../../recoil/date/selector';
 
 export const useShapedDate = () => {
+    const { closeModal } = useModal();
     const [shapedDate, setShapedDate] = useRecoilState(shapedDateState);
 
     const changeDate = (sign) => {
         const [NEXT, PREV] = [1, -1];
         const value = sign === '+' ? NEXT : PREV;
         setShapedDate(value);
+        closeModal('categoryTransaction');
     };
 
     return [shapedDate, changeDate];
