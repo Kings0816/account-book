1ff2daac180ee08a7e6013a45f04eb664a38c522
jhLim97 <wnsgur2749@naver.com>
2022-02-17 00:10:57 +0900

    test: 수입카테고리 영역만 필터링해주는 category selector에 대한 테스트코드 작성

---
+++ frontend/src/recoil/category/test.js
@@ -0,0 +1,33 @@
+import { snapshot_UNSTABLE } from 'recoil';
+
+import { categoryState } from './atom';
+import { filteredCategoryState } from './selector';
+
+const TEST_DATA = [
+    { id: 1, name: '미분류', color: '#817DCE', sign: 'all' },
+    { id: 2, name: '카페/간식', color: '#D092E2', sign: '-' },
+    { id: 4, name: '용돈', color: '#E6D267', sign: '+' },
+    { id: 5, name: '교통', color: '#94D3CC', sign: '-' },
+    { id: 10, name: '월급', color: '#B9D58C', sign: '+' },
+    { id: 13, name: '식비', color: '#4CA1DE', sign: '-' },
+    { id: 14, name: '여행/숙박', color: '#E2B765', sign: '-' },
+    { id: 28, name: '여가/취미', color: '#B9D58C', sign: '-' },
+    { id: 29, name: '교육', color: '#F4A460', sign: '-' },
+];
+
+const INCOME_DATA = [
+    { id: 2, name: '카페/간식', color: '#D092E2', sign: '-' },
+    { id: 5, name: '교통', color: '#94D3CC', sign: '-' },
+    { id: 13, name: '식비', color: '#4CA1DE', sign: '-' },
+    { id: 14, name: '여행/숙박', color: '#E2B765', sign: '-' },
+    { id: 28, name: '여가/취미', color: '#B9D58C', sign: '-' },
+    { id: 29, name: '교육', color: '#F4A460', sign: '-' },
+];
+
+describe('category Selectors 테스트', () => {
+    it('filteredCategoryState(수입 카테고리 영역만 필터링해주는) Selector 테스트', async () => {
+        const testSnapshot = snapshot_UNSTABLE(({ set }) => set(categoryState, TEST_DATA));
+        const result = testSnapshot.getLoadable(filteredCategoryState).valueOrThrow();
+        expect(result).toEqual(INCOME_DATA);
+    });
+});
